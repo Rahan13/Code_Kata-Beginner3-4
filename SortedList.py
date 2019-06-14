933bcd51f928f37f2ec324ffba4a1e8520c4e320
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-06-14 20:28:52 +0530

    Update and rename SortedList.py to Time.py

--- SortedList.py
+++
@@ -1,5 +0,0 @@
-n =int(input())
-a =input().split()
-b =list(map(int,a))
-b = sorted(b)
-[print(i, end=" ") for i in b]
---
+++ Time.py
@@ -0,0 +1,4 @@
+n =int(input())
+hrs = int(n/60)
+mins = int((n-(hrs*60)))
+print(hrs,mins)
