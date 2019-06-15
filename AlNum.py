069d89d2351b4f6d155a839e21b5d62e6409171f
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-06-15 20:30:06 +0530

    Update AlNum.py

--- AlNum.py
+++ AlNum.py
@@ -1,5 +1,15 @@
-str=input()
-if str.isalnum():
+n=input().split()
+n = "".join(n)
+cnum=0
+anum=0
+for i in range(0,len(n)):
+    if n[i].isnumeric():
+        cnum+=1
+        continue
+    elif n[i].isalpha():
+        anum+=1
+        continue
+if(anum>0 and cnum>0):
     print("yes")
 else:
     print("no")
