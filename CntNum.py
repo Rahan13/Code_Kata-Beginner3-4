4f51eb74011684975b4aa95f17ee69b941de172f
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-06-15 20:39:34 +0530

    Update and rename InstNum.py to CntNum.py

---
+++ CntNum.py
@@ -0,0 +1,6 @@
+num = int(input())
+count = 0
+while(num>0):
+    count+=1
+    num=int(num/10)
+print(count)
--- InstNum.py
+++
@@ -1,6 +0,0 @@
-n=input().split()
-a=input().split()
-if a.count(n[1])>0:
-    print("yes")
-else:
-    print("no")
