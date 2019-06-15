0deb863db14af5371648c7418cf22fc1290fc4f1
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-06-15 20:18:13 +0530

    Update and rename EvenNum.py to EvenOdd.py

--- EvenNum.py
+++
@@ -1,6 +0,0 @@
-a=int(input())
-b=a%2
-if(b==0):
-    print(a)
-else:
-    print(a-1)
---
+++ EvenOdd.py
@@ -0,0 +1,6 @@
+n=input().split()
+b=int(n[0])*int(n[1])
+if(b%2==0):
+    print("even")
+else:
+    print("odd")
