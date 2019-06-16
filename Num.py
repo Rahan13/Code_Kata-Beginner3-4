96c4f8b22bc103448aa25e74ca067eb47fe50004
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-06-16 23:54:34 +0530

    Update and rename NumPri.py to Num.py

---
+++ Num.py
@@ -0,0 +1,5 @@
+a=int(input())
+if(a%10==0):
+    print(a)
+else:
+    print((int(a/10)+1)*10)
--- NumPri.py
+++
@@ -1,9 +0,0 @@
-a=int(input())
-count=0
-for j in range(1,a+1):
-    if(a%j == 0):
-        count += 1
-if count==2:
-    print("yes")
-else:
-    print("no")
