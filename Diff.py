410865d8b35c0f0dc33fe2cdd562f8e62efce8bf
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-06-17 00:14:16 +0530

    Update and rename Mul7.py to Diff.py

---
+++ Diff.py
@@ -0,0 +1,6 @@
+a=input().split()
+diff=abs(int(a[0])-int(a[1]))
+if diff%2==0:
+    print("even")
+else:
+    print("odd")
--- Mul7.py
+++
@@ -1,6 +0,0 @@
-numb=int(input())
-a=numb
-if(a%7==0):
-    print("yes")
-else:
-    print("no")
