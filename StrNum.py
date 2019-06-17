7e42d0cc06a4952eafbcc140776455db070019b9
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-06-17 15:10:43 +0530

    Update StrNum.py

--- StrNum.py
+++ StrNum.py
@@ -1,5 +1,5 @@
-s=input()
-for i in range(0,len(s)):
-    if s[i].isnumeric():
-        print(s[i],end="")
-print("")
+s=input().split()
+s=list(map(int,s))
+sa=2*(s[0]*s[1]+s[1]*s[2]+s[2]*s[0])
+vol=s[0]*s[1]*s[2]
+print(sa,vol)
