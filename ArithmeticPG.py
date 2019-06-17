76cdfd72b6743e19d18e72074b6184711d1043b9
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-06-17 15:37:35 +0530

    Update and rename ArithmeticPg.py to ArithmeticPG.py

---
+++ ArithmeticPG.py
@@ -0,0 +1,7 @@
+x =input().split()
+n =int(x[0])
+a =int(x[1])
+d =int(x[2])
+l = a+(n-1)*d
+sum = int((n*(a+l))/2)
+print(sum)
--- ArithmeticPg.py
+++
@@ -1,7 +0,0 @@
-b =input().split()
-n =int(b[0])
-a =int(b[1])
-d =int(b[2])
-l = a+(n-1)*d
-sum = int((n*(a+l))/2)
-print(sum)
