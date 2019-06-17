de09fb1e3cca2a743f6c581746aabbe6d74c5d7b
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-06-17 23:14:59 +0530

    Update and rename ProdDigi.py to OrdChng.py

---
+++ OrdChng.py
@@ -0,0 +1,6 @@
+s=input().split()
+for i in range(0,len(s)-1):
+    if int(s[i]) > int(s[i+1]):
+        print(i)
+        break
+print("")
--- ProdDigi.py
+++
@@ -1,6 +0,0 @@
-s=int(input())
-prod=1
-while(s>0):
-    prod=prod*(s%10)
-    s=int(s/10)
-print(prod)
