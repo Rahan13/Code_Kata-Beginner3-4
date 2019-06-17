eb8d9a85f4b489cc34b8f9bc5dad2e1bfc6100b8
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-06-17 15:15:13 +0530

    Update Cuboid.py

--- Cuboid.py
+++ Cuboid.py
@@ -1,5 +1,4 @@
-s=input().split()
-s=list(map(int,s))
-sa=2*(s[0]*s[1]+s[1]*s[2]+s[2]*s[0])
-vol=s[0]*s[1]*s[2]
-print(sa,vol)
+s=input()
+num=input().split()
+num=list(map(int,num))
+print(sum(num))
